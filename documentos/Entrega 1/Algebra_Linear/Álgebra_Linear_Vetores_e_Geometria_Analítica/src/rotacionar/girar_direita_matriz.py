fefa3c359b5b0db40cd6b6bc98d7316e236fc08c
LiuYueLYHX <eduardochenzou@gmail.com>
2026-03-26 00:08:05 -0300

    entrega entrega 1 do Algebra

--- documentos/Entrega 1/Algebra_Linear/Álgebra_Linear_Vetores_e_Geometria_Analítica/src/rotacionar/girar_direita_matriz.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("Matriz_Original.csv", sep=";")
-
-df[["linha", "coluna"]] = df[["coluna", "linha"]]
-
-df.to_csv("Matriz_GirarDireita.csv", index=False, sep=";")
